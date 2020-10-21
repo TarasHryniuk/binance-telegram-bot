082889dd6da16e6ddad4e674ddaebaed94f1aa4d
ReZgOTbM <#kUDMefD6=>
2020-10-21 22:51:04 +0300

    start project. initialize telegram bot

---
+++ src/main/java/bot/crypto/App.java
@@ -0,0 +1,38 @@
+package bot.crypto;
+
+import bot.crypto.telegram.Bot;
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.ApiContextInitializer;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author Taras Hryniuk, created on  21.10.2020
+ * email : hryniuk.t@gmail.com
+ */
+public class App {
+    private static final Logger LOGGER = Logger.getLogger(App.class);
+    static Properties appProps;
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.initializeProperties();
+        String appVersion = appProps.getProperty("api.key");
+        ApiContextInitializer.init();
+        Bot test_habr_bot = new Bot("goover_bot", appVersion);
+        test_habr_bot.botConnect();
+    }
+
+    private void initializeProperties() {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "app.properties";
+        appProps = new Properties();
+        try {
+            appProps.load(new FileInputStream(appConfigPath));
+        } catch (IOException e) {
+            LOGGER.error(e);
+        }
+    }
+}
---
+++ src/main/java/bot/crypto/telegram/Bot.java
@@ -0,0 +1,81 @@
+package bot.crypto.telegram;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+/**
+ * @author Taras Hryniuk, created on  21.10.2020
+ * email : hryniuk.t@gmail.com
+ */
+public class Bot extends TelegramLongPollingBot {
+
+    private static final Logger LOGGER = Logger.getLogger(Bot.class);
+
+    final int RECONNECT_PAUSE = 10000;
+
+    @Setter
+    @Getter
+    String userName;
+    @Setter
+    @Getter
+    String token;
+
+    public Bot(String userName, String token) {
+        this.userName = userName;
+        this.token = token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        LOGGER.info("Receive new Update. updateID: " + update.getUpdateId());
+
+        Long chatId = update.getMessage().getChatId();
+        String inputText = update.getMessage().getText();
+
+        if (inputText.startsWith("/start")) {
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("Hello. This is start message");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                LOGGER.error(e);
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return userName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    public void botConnect() {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(this);
+            LOGGER.info("TelegramAPI started. Look for messages");
+        } catch (TelegramApiRequestException e) {
+            LOGGER.error("Cant Connect. Pause " + RECONNECT_PAUSE / 1000 + "sec and try again. Error: " + e.getMessage());
+            try {
+                Thread.sleep(RECONNECT_PAUSE);
+            } catch (InterruptedException e1) {
+                LOGGER.error(e1);
+                return;
+            }
+            botConnect();
+        }
+    }
+
+}
